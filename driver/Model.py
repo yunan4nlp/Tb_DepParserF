03cf9ea2255301c71154e62cf4d274288f8ac1c5
YuNan <yunan.hlju@gmail.com>
2018-03-29 19:30:24 +0800

    first commit

---
+++ driver/Model.py
@@ -0,0 +1,121 @@
+from driver.Layer import *
+from data.Vocab import *
+import time
+
+
+def drop_input_independent(word_embeddings, tag_embeddings, dropout_emb):
+    batch_size, seq_length, _ = word_embeddings.size()
+    word_masks = word_embeddings.data.new(batch_size, seq_length).fill_(1 - dropout_emb)
+    word_masks = Variable(torch.bernoulli(word_masks), requires_grad=False)
+    tag_masks = tag_embeddings.data.new(batch_size, seq_length).fill_(1 - dropout_emb)
+    tag_masks = Variable(torch.bernoulli(tag_masks), requires_grad=False)
+    scale = 3.0 / (2.0 * word_masks + tag_masks + 1e-12)
+    word_masks *= scale
+    tag_masks *= scale
+    word_masks = word_masks.unsqueeze(dim=2)
+    tag_masks = tag_masks.unsqueeze(dim=2)
+    word_embeddings = word_embeddings * word_masks
+    tag_embeddings = tag_embeddings * tag_masks
+
+    return word_embeddings, tag_embeddings
+
+def drop_sequence_sharedmask(inputs, dropout, batch_first=True):
+    if batch_first:
+        inputs = inputs.transpose(0, 1)
+    seq_length, batch_size, hidden_size = inputs.size()
+    drop_masks = inputs.data.new(batch_size, hidden_size).fill_(1 - dropout)
+    drop_masks = Variable(torch.bernoulli(drop_masks), requires_grad=False)
+    drop_masks = drop_masks / (1 - dropout)
+    drop_masks = torch.unsqueeze(drop_masks, dim=2).expand(-1, -1, seq_length).permute(2, 0, 1)
+    inputs = inputs * drop_masks
+
+    return inputs.transpose(1, 0)
+
+
+class Encoder(nn.Module):
+    def __init__(self, vocab, config, pretrained_embedding):
+        super(Encoder, self).__init__()
+        self.config = config
+        self.word_embed = nn.Embedding(vocab.vocab_size, config.word_dims, padding_idx=0)
+        self.extword_embed = nn.Embedding(vocab.extvocab_size, config.word_dims, padding_idx=0)
+        self.tag_embed = nn.Embedding(vocab.tag_size, config.tag_dims, padding_idx=0)
+
+        word_init = np.zeros((vocab.vocab_size, config.word_dims), dtype=np.float32)
+        self.word_embed.weight.data.copy_(torch.from_numpy(word_init))
+        
+        tag_init = np.random.randn(vocab.tag_size, config.tag_dims).astype(np.float32)
+        self.tag_embed.weight.data.copy_(torch.from_numpy(tag_init))
+
+        self.extword_embed.weight.data.copy_(torch.from_numpy(pretrained_embedding))
+        self.extword_embed.weight.requires_grad = False
+
+
+        self.lstm = MyLSTM(
+            input_size=config.word_dims + config.tag_dims,
+            hidden_size=config.lstm_hiddens,
+            num_layers=config.lstm_layers,
+            batch_first=True,
+            bidirectional=True,
+            dropout_in = config.dropout_lstm_input,
+            dropout_out=config.dropout_lstm_hidden,
+        )
+
+    def forward(self, words, extwords, tags, masks):
+        # x = (batch size, sequence length, dimension of embedding)
+        x_word_embed = self.word_embed(words)
+        x_extword_embed = self.extword_embed(extwords)
+        x_embed = x_word_embed + x_extword_embed
+        x_tag_embed = self.tag_embed(tags)
+
+        if self.training:
+            x_embed, x_tag_embed = drop_input_independent(x_embed, x_tag_embed, self.config.dropout_emb)
+
+        x_lexical = torch.cat((x_embed, x_tag_embed), dim=2)
+
+        outputs, _ = self.lstm(x_lexical, masks, None)
+        outputs = outputs.transpose(1, 0)
+
+        if self.training:
+            outputs = drop_sequence_sharedmask(outputs, self.config.dropout_mlp)
+        return outputs
+
+class Decoder(nn.Module):
+    def __init__(self, vocab, config):
+        super(Decoder, self).__init__()
+        self.vocab = vocab
+        self.config = config
+        self.output = nn.Linear(in_features= config.lstm_hiddens * 2 * 4,
+                                out_features=len(vocab._id2ac))
+
+    def forward(self, batch_hidden_state, candidate_actions):
+        assert batch_hidden_state.size()[0] == len(candidate_actions)
+        outputs = self.output.forward(batch_hidden_state)
+        for (idx, actions) in enumerate(candidate_actions):
+            candidate_index = set()
+            for (idy, ac) in enumerate(actions):
+                index = self.vocab._ac2id[ac]
+                assert index < len(self.vocab._ac2id)
+                candidate_index.add(index)
+            for idy in range(0, self.vocab.ac_size):
+                if idy not in candidate_index:
+                    outputs.data[idx][idy] = -1e+20
+        return outputs
+
+'''
+class Table(nn.Embedding):
+    def __init__(self, out_dim, alpha):
+        self.alpha = alpha
+        super(Table, self).__init__(embedding_dim=out_dim,
+                                    num_embeddings=len(alpha),
+                                    )
+        self.weight.requires_grad = True
+
+    def forward(self, input):
+        num = len(input)
+        x = Variable(torch.LongTensor(num))
+        for (idx, iter) in enumerate(input):
+            index = self.alpha[iter]
+            x.data[idx] = index
+        output = super(Table,self).forward(x)
+        return output
+'''
---
+++ driver/Parser.py
@@ -0,0 +1,197 @@
+from transition.State import *
+import numpy as np
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+class TransitionBasedParser(object):
+    def __init__(self, encoder, decoder, root_id, config):
+        self.config = config
+        self.encoder = encoder
+        self.decoder = decoder
+        self.root = root_id
+        encoder_p = next(filter(lambda p: p.requires_grad, encoder.parameters()))
+        self.use_cuda = encoder_p.is_cuda
+        self.bucket = Variable(torch.zeros(self.config.train_batch_size, 1, self.config.lstm_hiddens * 2)).type(torch.FloatTensor)
+        self.index = Variable(torch.zeros(self.config.train_batch_size * 4)).type(torch.LongTensor)
+        self.device = encoder_p.get_device() if self.use_cuda else None
+        if self.use_cuda:
+            self.bucket = self.bucket.cuda(self.device)
+            self.index = self.index.cuda(self.device)
+        self.gold_pred_pairs = []
+        self.training = True
+        if self.config.train_batch_size > self.config.test_batch_size:
+            batch_size = self.config.train_batch_size
+        else:
+            batch_size = self.config.test_batch_size
+        self.batch_states = []
+        self.step = []
+        for idx in range(0, batch_size):
+            self.batch_states.append([])
+            self.step.append(0)
+            for idy in range(0, 1024):
+                self.batch_states[idx].append(State())
+
+    def encode(self, words, extwords, tags, masks):
+        if self.use_cuda:
+            words, extwords = words.cuda(self.device), extwords.cuda(self.device),
+            tags = tags.cuda(self.device)
+            masks = masks.cuda(self.device)
+        self.encoder_outputs = self.encoder.forward(words, extwords, tags, masks)
+
+    def compute_loss(self, true_acs):
+        b, l1, l2 = self.decoder_outputs.size()
+        true_acs = _model_var(
+            self.encoder,
+            pad_sequence(true_acs, length=l1, padding=-1, dtype=np.int64))
+        arc_loss = F.cross_entropy(
+            self.decoder_outputs.view(b * l1, l2), true_acs.view(b * l1),
+            ignore_index=-1)
+        return arc_loss
+
+    def compute_accuracy(self):
+        total_num = 0
+        correct = 0
+        for iter in self.gold_pred_pairs:
+            gold_len = len(iter[0])
+            pred_len = len(iter[1])
+            assert gold_len == pred_len
+            total_num += gold_len
+            for idx in range(0, gold_len):
+                if iter[0][idx] == iter[1][idx]:
+                    correct += 1
+        return total_num, correct
+
+
+    def decode(self, batch_data, batch_gold_states, bacth_gold_actions, vocab):
+        decoder_scores = []
+        self.gold_pred_pairs.clear()
+
+        self.b, self.l1, self.l2 = self.encoder_outputs.size()
+        if self.b != self.bucket.size()[0]:
+            self.bucket = Variable(torch.zeros(self.b, 1, self.l2)).type(torch.FloatTensor)
+            if self.use_cuda:
+                self.bucket = self.bucket.cuda(self.device)
+        self.encoder_outputs = torch.cat((self.encoder_outputs, self.bucket), 1)
+        for idx in range(0, self.b):
+            start_state = self.batch_states[idx][0]
+            start_state.clear()
+            start_state.ready(batch_data[idx], vocab)
+            self.step[idx] = 0
+
+        while not self.all_states_are_finished():
+            hidden_states = self.batch_prepare()
+            if self.training:
+                gold_actions = self.get_gold_actions(bacth_gold_actions)
+            all_candidates = self.get_candidates(vocab)
+            action_scores = self.decoder.forward(hidden_states, all_candidates)
+            pred_ac_ids = self.get_predicted_ac_id(action_scores)
+            pred_actions = self.get_predict_actions(pred_ac_ids, vocab)
+            if self.training:
+                self.move(gold_actions, vocab)
+                self.gold_pred_pairs.append((gold_actions, pred_actions))
+                decoder_scores.append(action_scores.unsqueeze(1))
+            else:
+                self.move(pred_actions, vocab)
+        if self.training:
+            self.decoder_outputs = torch.cat(decoder_scores, 1)
+
+    def get_gold_actions(self, batch_gold_actions):
+        gold_actions = []
+        for idx in range(0, self.b):
+            cur_states = self.batch_states[idx]
+            cur_step = self.step[idx]
+            if not cur_states[cur_step].is_end():
+                gold_ac = batch_gold_actions[idx][cur_step]
+                gold_actions.append(gold_ac)
+        return gold_actions
+
+    def get_predict_actions(self, pred_ac_ids, vocab):
+        pred_actions = []
+        for ac_id in pred_ac_ids:
+            pred_ac = vocab.id2ac(ac_id)
+            pred_actions.append(pred_ac)
+        return pred_actions
+
+    def all_states_are_finished(self):
+        is_finish = True
+        for idx in range(0, self.b):
+            cur_states = self.batch_states[idx]
+            if not cur_states[self.step[idx]].is_end():
+                is_finish = False
+                break
+        return is_finish
+
+    def batch_prepare(self):
+        if self.b != self.index.size()[0]:
+            self.index = Variable(torch.zeros(self.b * 4)).type(torch.LongTensor)
+            if self.use_cuda:
+                self.index = self.index.cuda(self.device)
+        for idx in range(0, self.b):
+            cur_states = self.batch_states[idx]
+            cur_step = self.step[idx]
+            if not cur_states[cur_step].is_end():
+                s0, s1, s2, q0 = cur_states[cur_step].prepare_index(self.l1)
+                offset_x = idx * 4
+                offset_y = idx * (self.l1 + 1)
+                self.index.data[offset_x + 0] = s0 + offset_y
+                self.index.data[offset_x + 1] = s1 + offset_y
+                self.index.data[offset_x + 2] = s2 + offset_y
+                self.index.data[offset_x + 3] = q0 + offset_y
+        h_s = torch.index_select(self.encoder_outputs.view(self.b * (self.l1 + 1), self.l2), 0, self.index)
+        h_s = h_s.view(self.b, 4 * self.l2)
+        return h_s
+
+    def get_predicted_ac_id(self, action_scores):
+        ac_ids = []
+        for idx in range(0, self.b):
+            cur_states = self.batch_states[idx]
+            if not cur_states[self.step[idx]].is_end():
+                ac_id = np.argmax(action_scores.data[idx])
+                ac_ids.append(ac_id)
+        return ac_ids
+
+    def move(self, pred_actions, vocab):
+        count = 0
+        for idx in range(0, self.b):
+            cur_states = self.batch_states[idx]
+            if not cur_states[self.step[idx]].is_end():
+                count += 1
+        assert len(pred_actions) == count
+        offset = 0
+        for idx in range(0, self.b):
+            cur_states = self.batch_states[idx]
+            cur_step = self.step[idx]
+            if not cur_states[cur_step].is_end():
+                next_state = self.batch_states[idx][cur_step + 1]
+                cur_states[cur_step].move(next_state, pred_actions[offset])
+                cur_states.append(next_state)
+                offset += 1
+                self.step[idx] += 1
+
+
+    def get_candidates(self, vocab):
+        all_candidates = []
+        for idx in range(0, self.b):
+            cur_states = self.batch_states[idx]
+            cur_step = self.step[idx]
+            if not cur_states[cur_step].is_end():
+                candidates = cur_states[cur_step].get_candidate_actions(vocab)
+                all_candidates.append(candidates)
+            else:
+                all_candidates.append([])
+        return all_candidates
+
+def _model_var(model, x):
+    p = next(filter(lambda p: p.requires_grad, model.parameters()))
+    if p.is_cuda:
+        x = x.cuda(p.get_device())
+    return torch.autograd.Variable(x)
+
+def pad_sequence(xs, length=None, padding=-1, dtype=np.float64):
+    lengths = [len(x) for x in xs]
+    if length is None:
+        length = max(lengths)
+    y = np.array([np.pad(x.astype(dtype), (0, length - l),
+                         mode="constant", constant_values=padding)
+                  for x, l in zip(xs, lengths)])
+    return torch.from_numpy(y)
